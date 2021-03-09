b348de9f564bfd9efaa1406cc051a1bf76222a2d
lyca22 <ariep2206@gmail.com>
2021-03-09 17:51:27 -0500

    Created fxml files for the UI

--- src/model/Ingredient.java
+++ src/model/Ingredient.java
@@ -2,4 +2,18 @@ package model;
 
 public class Ingredient {
 
+	private String name;
+
+	public Ingredient(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
