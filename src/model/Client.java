ed18b976e687e424179175350da731461b4076df
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-03-10 15:31:00 -0500

    Class client

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,5 +1,15 @@
 package model;
 
 public class Client {
+	private String name;
+	private String surname;
+	private int iD;
+	private String address;
+	private int phoneNumber;
+	private String observations;
+	
+	public Client (String name, String surname, int iD, String address, int phoneNumber, String observations) {
+		
+	}
 
 }
